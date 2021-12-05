6f92ee45709ba6448548c6f41401e1a25128c3ab
Samson <sbadlia2002@gmail.com>
2021-12-04 22:12:14 -0500

    Buttons cannot be done using while loop so brute forced them

--- calculator.py
+++ calculator.py
@@ -43,40 +43,48 @@ if __name__ == "__main__":
     exp_field = Entry(gui, textvariable=equation)
     exp_field.grid(columnspan=4, ipadx=length/4)
 
-    number = 1
-    r = None
-    c = None
-    while number < 11:
-        button = Button(gui, text=str(number), fg='white', bg='black',command=lambda: press(number%10), height=int(width/100), width=int(length/100))
-        if number < 11:
-            r = 4
-            if number < 7:
-                r = 3
-                if number < 4:
-                    r = 2
-        if (number - 1) % 3 == 0:
-            c = 0
-        if (number) % 3 == 2:
-            c = 1
-        if (number) % 3 == 0:
-            c = 2
-        if (number) == 10:
-            r=5
-            c=0
-        number += 1
-        button.grid(row=r,column = c)
 
-    plus = Button(gui, text=' + ', fg='white', bg='black',command=lambda: press("+"), height=int(width/100), width=int(length/100))
-    plus.grid(row=2, column=3)
+    one = Button(gui, text=' 1 ', fg='white', bg='black',command=lambda: press(1), height=int(width/100), width=int(length/100))
+    one.grid(row=2, column=0)
 
-    minus = Button(gui, text=' - ', fg='white', bg='black',command=lambda: press("-"), height=int(width/100), width=int(length/100))
-    minus.grid(row=3, column=3)
+    two = Button(gui, text=' 2 ', fg='white', bg='black',command=lambda: press(2), height=int(width/100), width=int(length/100))
+    two.grid(row=2, column=1)
 
-    multiply = Button(gui, text=' * ', fg='white', bg='black',command=lambda: press("*"), height=int(width/100), width=int(length/100))
-    multiply.grid(row=4, column=3)
+    three = Button(gui, text=' 3 ', fg='white', bg='black',command=lambda: press(3), height=int(width/100), width=int(length/100))
+    three.grid(row=2, column=2)
 
-    divide = Button(gui, text=' / ', fg='white', bg='black',command=lambda: press("/"), height=int(width/100), width=int(length/100))
-    divide.grid(row=5, column=3)
+    four = Button(gui, text=' 4 ', fg='white', bg='black',command=lambda: press(4), height=int(width/100), width=int(length/100))
+    four.grid(row=3, column=0)
+
+    five = Button(gui, text=' 5 ', fg='white', bg='black',command=lambda: press(5), height=int(width/100), width=int(length/100))
+    five.grid(row=3, column=1)
+
+    six = Button(gui, text=' 6 ', fg='white', bg='black',command=lambda: press(6), height=int(width/100), width=int(length/100))
+    six.grid(row=3, column=2)
+
+    seven = Button(gui, text=' 7 ', fg='white', bg='black',command=lambda: press(7), height=int(width/100), width=int(length/100))
+    seven.grid(row=4, column=0)
+
+    eight = Button(gui, text=' 8 ', fg='white', bg='black',command=lambda: press(8), height=int(width/100), width=int(length/100))
+    eight.grid(row=4, column=1)
+
+    nine = Button(gui, text=' 9 ', fg='white', bg='black',command=lambda: press(9), height=int(width/100), width=int(length/100))
+    nine.grid(row=4, column=2)
+
+    zero = Button(gui, text=' 0 ', fg='white', bg='black',command=lambda: press(0), height=int(width/100), width=int(length/100))
+    zero.grid(row=5, column=0)
+
+    addition = Button(gui, text=' + ', fg='white', bg='black',command=lambda: press("+"), height=int(width/100), width=int(length/100))
+    addition.grid(row=2, column=3)
+
+    subtraction = Button(gui, text=' - ', fg='white', bg='black',command=lambda: press("-"), height=int(width/100), width=int(length/100))
+    subtraction.grid(row=3, column=3)
+
+    multiplication = Button(gui, text=' * ', fg='white', bg='black',command=lambda: press("*"), height=int(width/100), width=int(length/100))
+    multiplication.grid(row=4, column=3)
+
+    division = Button(gui, text=' / ', fg='white', bg='black',command=lambda: press("/"), height=int(width/100), width=int(length/100))
+    division.grid(row=5, column=3)
 
     equal = Button(gui, text=' = ', fg='white', bg='black',command=equalpress, height=int(width/100), width=int(length/100))
     equal.grid(row=5, column=2)
